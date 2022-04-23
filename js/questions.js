0b0d18fceaff3667d4b3dc3e81066ef237873d0a
scalefy <scalefy@gmail.com>
2022-04-23 16:51:05 -0300

    repo1

---
+++ js/questions.js
@@ -0,0 +1,189 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Quais são as chaves de uma boa experiência na Viagem do Cliente?",
+    answer: "Um ambiente agradável e aconchegante, pedidos simples, eficcazes e rápidos, uma experiência extraordinária com serviço de entrega a mesa e um espaço estimulante.",
+    options: [
+      "Um espaço estimulante, velocidade e confiabilidade.",
+      "um significado social, velocidade espaço estimulante.",
+      "Um ambiente agradável e aconchegante, pedidos simples, eficcazes e rápidos, uma experiência extraordinária com serviço de entrega a mesa e um espaço estimulante.",
+      "Todas as alternativas."
+    ]
+  },
+    {
+      numb: 2,
+      question: "Quais são os três grandes eixos que vão ajudar você a conseguir experiências memoráveis?",
+      answer: "Observar, Interagir e Intervir",
+      options: [
+        "Empatia, Conexão e Interagir.",
+        "Observar, interagir e Ser Cortês.",
+        "Nenhuma das alternativas.",
+        "Observar, Interagir e Intervir"
+    ]
+  },
+    {
+    
+      numb: 3,
+      question: "Quais são os níveis da pirâmide de LSM?",
+      answer: "QSLV, visibilidade e acesso ao restaurante, execução de promoção nacional, análise Trading Area, relações com comunidade e desenvolvimento de vendas locais",
+      options: [
+        "QSLV, visibilidade e acesso ao restaurante, foco em vendas e GCs, entrega de BOGs, Eventos locais e FOCO em Vendas",
+        "QSLV, visibilidade e acesso ao restaurante, desenvolvimento do LSM, entrega de BOGs, Bom Vizinho.",
+        "QSLV, visibilidade e acesso ao restaurante, execução de promoção nacional, análise Trading Area, relações com comunidade e desenvolvimento de vendas locais",
+        "QSLV, visibilidade e acesso ao restaurante, McFesta e Vnedas Locais"
+          ]
+  },
+    {
+    
+      numb: 4,
+      question: "Quais são as 6 tecnicas do influencers Vendedores?",
+      answer: "Gostamos de Surpreender, Vivenciamos as mesmas coisas, Somos acolhedores, criamos tendência, Somos especialistas, Criamos Oportunidades",
+      options: [
+        "Gostamos de Surpreender, Sugestão de Vendas, Somos acolhedores, criamos tendência, Somos especialistas, Criamos Oportunidades",
+        "Gostamos de Surpreender, Vivenciamos as mesmas coisas, Oferecer Adicionais, criamos tendência, Somos especialistas, Criamos Oportunidades.",
+        "Gostamos de Surpreender, Vivenciamos as mesmas coisas, Somos acolhedores, criamos tendência, Somos especialistas, Venda de Sobremesas no salão.",
+        "Gostamos de Surpreender, Vivenciamos as mesmas coisas, Somos acolhedores, criamos tendência, Somos especialistas, Criamos Oportunidades",
+      ]
+  },
+    {
+      numb: 5,
+      question: "Quais os três pilares que sustentam a Missão do Embaixador da Experiência do Cliente?",
+      answer: "Experiência da Viagem do Cliente, Experiência Familiar e Construção de Vendas.",
+      options: [
+        "Experiência da Viagem do Cliente, Princípio de Vendas e Construção de Vendas",
+        "Conexão com o cliente, Experiência Familiar e Construção de Vnedas.",
+        "Nenhuma das alternativas.",
+        "Experiência da Viagem do Cliente, Experiência Familiar e Construção de Vendas."
+    ]
+  },
+   {
+    
+      numb: 6,
+      question: "O que espera-se da atuação do Embaixador da Experiência do Cliente, como prioridade?",
+      answer: "Oferecer uma melhor experiência para cada consumidor, tornando aquele momento único",
+      options: [
+        "Realizar limpeza das mesas",
+        "Realizar atividades de pessoa do caixa",
+        "Estocar brindes na área dos caixas",
+        "Oferecer uma melhor experiência para cada consumidor, tornando aquele momento único",
+      ]
+  },
+  {
+
+      numb: 7,
+      question: "Em resumo qual a principal preocupação que o Embaixador da Experiência do Cliente precisa ter com o apoio na manutenção do material de marketing?",
+      answer: "Revisar as peças de comunicação de acordo com os guias de implementação, assegurando de que estejam limpas e em bom estado de conservação, notificar caso estejam danificadas ou seja necessário trocá-las",
+      options: [
+        "Manter guardado o material de material de marketing em ambiente exclusivo",
+        "Posicionar todos os materiais de material de marketing de acordo com seu conhecimento",
+        "Somente posicionar os materiais de material de marketing quando o assistente de trade marketing estiver no restaurante",
+        "Revisar as peças de comunicação de acordo com os guias de implementação, assegurando de que estejam limpas e em bom estado de conservação, notificar caso estejam danificadas ou seja necessário trocá-las",
+      ]
+  },
+  {
+    numb: 8,
+    question: "Qual atuação do Embaixador da Experiência do cliente no momento ESPERAR PARA FAZER O PEDIDO? ",
+    answer: "Criar vínculos, oferecer um produto, fornecer informações sobre o EOTF.",
+    options: [
+      "Criar Vínculos, oferecer um produto, venda além do balcão, fornecer informações sobre EOTF",
+      "Hospitalidade, Entretenimento com a familia, venda além do balcão, apoio na manutenção do material de marketing, tour e celebração de festa de aniversário.",
+      "Nenhuma das alternativas",
+      "Criar vínculos, oferecer um produto, fornecer informações sobre o EOTF.",
+     ]
+  },
+  {
+    numb: 9,
+    question: "Quais são os 9 momentos denominados como Trajeto da Viagem do Cliente?",
+    answer: "Chega no Restaurante, Entra no Restaurante, Espera para fazer o pedido, Paga e espera o pedido, Retira o pedido, Come, Tem um tempo só dele, Sai do restaurante e Reflexão sobre a visita",
+    options: [
+      "Chega no Restaurante, Entra no Restaurante, Espera para fazer o pedido, Paga , Retira o pedido, Come, Tem um tempo só dele, Sai do restaurante e Reflexão sobre a visita",
+      "Entra no Restaurante, Espera para fazer o pedido, Paga e espera o pedido, Retira o pedido, Come, Tem um tempo só dele, Sai do restaurante",
+      "Nenhuma das alternativas",
+      "Chega no Restaurante, Entra no Restaurante, Espera para fazer o pedido, Paga e espera o pedido, Retira o pedido, Come, Tem um tempo só dele, Sai do restaurante e Reflexão sobre a visita",
+    ]
+  },
+  {
+
+    numb: 10,
+    question: "Quais são os três princípios da Experiência Familiar?",
+    answer: "Facilitar, Tornar divertido, Fazer com que cada família sinta-se especial.",
+    options: [
+      "Tornar divertido, Lugar agradavel, Fazer com que cada família sinta-se especial. ",
+      "Facilitar, Tornar divertido, Fazer com que cada família sinta-se especial.",
+      "Tornar divertido, Lugar agradavel, Fazer com que cada família sinta-se única.",
+      "Facilitar, Lugar agradavel, Fazer com que cada família sinta-se única.",
+    ]
+  },
+  {
+
+    numb: 11,
+    question: "Quais são os objetivos do LSM?",
+    answer: "Aumentar a participação do restaurante na comunidade",
+    options: [
+      "Todas as alternativas",
+      "Aumentar a participação do restaurante na comunidade",
+      "Diferenciar o restaurante da concorrência.",
+      "Trazer personalidade local para a publicidade e as promoções nacionais.",
+    ]
+  },
+  {
+    numb: 12,
+    question: "Qual o propósito da Cooltura de Serviço?",
+    answer: "Gerar bons Momentos para as pessoas sendo nós mesmos",
+    options: [
+      "Manter uma postura séria o tempo todo, para gerar mais profissionalismo",
+      "Treinar os funcionários a ser educado e gentil",
+      "Todas as alternativas",
+      "Gerar bons Momentos para as pessoas sendo nós mesmos",
+    ]
+  },
+  {
+    numb: 13,
+    question: "Qual a missão do Embaixador da Experiência do Cliente?",
+    answer: "A missão da função do Embaixador da Experiência do Cliente está diretamente relacionada com nossa Cooltura de Serviço e com a plataforma EOTF. Com o objetivo de proporcionar a melhor experiência sendo impecáveis, simples, convenientes e divertidos. A função concretizará sua missão em três dimensões da Experiência do Cliente que também impactam no sucesso do negócio do restaurante: Viagem do Cliente, Experiência Familiar, Construção de Vendas",
+    options: [
+      "Entregar PDPs e realizar atividades com todas as crianças, tratar como se fossem estrelas",
+      "Promover, desenvolver, vender e executar festas de aniversários e comemorações escolares e empresariais",
+      "Suporte e monitoramento do material de marketing do restaurante e degustação de produtos a cada lançamento",
+      "A missão da função do Embaixador da Experiência do Cliente está diretamente relacionada com nossa Cooltura de Serviço e com a plataforma EOTF. Com o objetivo de proporcionar a melhor experiência sendo impecáveis, simples, convenientes e divertidos. A função concretizará sua missão em três dimensões da Experiência do Cliente que também impactam no sucesso do negócio do restaurante: Viagem do Cliente, Experiência Familiar, Construção de Vendas",
+    ]
+  },
+  {
+    numb: 14,
+    question: "No atendimento do totem, qual a preocupação do Embaixador da Experiencia do Cliente?",
+    answer: "Ajudar o cliente a concluir o pedido no totem, especialmente pais com filhos pequenos e idosos, utilizando as técnicas de Influencers Vendedores",
+    options: [
+      "Ficar 100% posicionado ao lado do totem.",
+      "Ficar apenas no horário do almoço e janta posicionado no totem.",
+      "Nenhuma das alternativas",
+      "Ajudar o cliente a concluir o pedido no totem, especialmente pais com filhos pequenos e idosos, utilizando as técnicas de Influencers Vendedores",
+    ]
+  },
+  {
+    numb: 14,
+    question: "Quais são os princípios centrais dentro da Viagem do Cliente?",
+    answer:"", 
+    options: [
+      "Conectar-se ao cliente, priorizar o cliente, servir a melhor comida e ser autêntico",
+      "Conectar-se ao cliente, priorizar o cliente, ser rápido e conveniente",
+      "Conectar-se ao cliente, priorizar o cliente",
+      "Atendimento rápido, cortês, fidelizar o cliente e servir a melhor comida",
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
